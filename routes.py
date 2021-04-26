a9af143121289de0500faa66e0061e906dcc037e
sakshi <sharia1@babson.edu>
2021-04-26 18:14:59 -0400

    Update routes.py

--- routes.py
+++ routes.py
@@ -35,11 +35,11 @@ class MyForm(FlaskForm):
             DataRequired()
         ]
     )
-    c_travel_type = SelectField('Car type', choices =[(' '),('Gas'), ('Electric'),('Plugin'),('Hybrid')])
-    t_travel_type = SelectField('Train type',choices = [(' '),('Electric'), ('Fossil Fuel')])
-    p_travel_type= SelectField('Plane type', choices=[(' '),('domestic'), ('long haul')] )
+    c_travel_type = SelectField('Car type', choices =[('none'),('Gas'), ('Electric'),('Plugin'),('Hybrid')])
+    t_travel_type = SelectField('Train type',choices = [('none'),('Electric'), ('Fossil Fuel')])
+    p_travel_type= SelectField('Plane type', choices=[('none'),('domestic'), ('long haul')] )
 
-    people = SelectField("If in a car, How many people were there?", choices=[(' '),('1'),('2'),('3'),('4')])
+    people = SelectField("If in a car, How many people were there?", choices=[('1'),('2'),('3'),('4')])
     
     submit_1 = SubmitField('Distance')
     submit_2 = SubmitField('CO2 Emission')
@@ -52,7 +52,7 @@ class MyForm(FlaskForm):
 def homepage():
     "this function is reading the html page to allow the user to fill out the form for a designated location"
     form = MyForm() 
-    # transportation = ["Train", "Bus", "Plane", "Car" ]
+    transportation = ["Train", "Bus", "Plane", "Car" ]
     
     return render_template('signin.html', form=form) 
 
@@ -61,7 +61,9 @@ def homepage():
 def dis_data():
     """ Based on the location given in the form, the data will go to PArt1 and get the closest stop and if it is wheelchair accessible"""
     location1 = request.form['start']
+    location1 = str(location1)
     location2 = request.form['end']
+    location2 = str(location2)
     result = lat_long(location1, location2)
 
     return render_template("data_presentation.html", result=result, location1=location1,location2=location2)
@@ -70,34 +72,29 @@ def dis_data():
 def co2_data():
     """Based on the data inputed and pressing CO2 Emission will convert the information to what was asked in 
     the last two functions. It will render the results in  to appear"""
-    em=0
-    location1 = request.form['start']
-    location2 = request.form['end']
-    train_type = request.form['t_travel_type']
-    car_type =request.form['c_travel_type']
-    car_type = str(car_type)
-    plane_type =request.form['p_travel_type']
-    plane_type = str(plane_type)
-    a_people = request.form['people']
-    a_people = str(a_people)
-    distance = lat_long(location1, location2)
-    typer = request.form['method']
-    typer = str(typer)
-    outcome, em_round = travel(typer,distance)
-    results= saved(typer,distance,em)
-    saved1,percent,mini,extra_saved,extra_percent = results
-    
-    return render_template('co2_data.html', outcome=outcome, em_round=em_round,results=results,saved1=saved1, mini=mini,percent=percent, extra_percent=extra_percent,extra_saved=extra_saved )
-
-
-
-
-
-
-
-
-
-
+    if request.method == "POST":
+        location1 = request.form['start']
+        location1 = str(location1)
+        location2 = request.form['end']
+        location2 = str(location2)
+        train_type = request.form['t_travel_type']
+        car_type =request.form['c_travel_type']
+        plane_type =request.form['p_travel_type']
+        a_people = request.form['people']
+        distance = lat_long(location1, location2)
+        typer = request.form['method']
+        typer = str(typer)
+        em =12
+        outcome = travel(typer,distance)
+        co2_save= saved(typer,distance,em)
+    if outcome is not None: 
+        if isinstance(outcome, str):
+            print(f"Hi {outcome}")
+            return render_template('co2_data.html', outcome=outcome, co2_save=co2_save,saved=saved)
+        else:
+            return render_template('co2_data.html', outcome=outcome, co2_save=co2_save, saved=saved)
+    return render_template('co2_data.html', error=True)
+return render_template('co2_data.html', error=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
